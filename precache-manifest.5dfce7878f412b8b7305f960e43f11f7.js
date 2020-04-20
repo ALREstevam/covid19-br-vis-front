f4424ceb941be9539dfaa1a3c0ab6068343185b8
ALREstevam <a166348@dac.unicamp.br>
2020-04-20 20:30:38 -0300

    Updates

---
+++ precache-manifest.5dfce7878f412b8b7305f960e43f11f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d605094573253e1ccc74cf4c5fd839db",
+    "url": "/covid19-br-vis-front/index.html"
+  },
+  {
+    "revision": "87a9217433054a6b1216",
+    "url": "/covid19-br-vis-front/static/css/main.7f51e2c4.chunk.css"
+  },
+  {
+    "revision": "757c803fae700f0ec9a6",
+    "url": "/covid19-br-vis-front/static/js/2.b629d9b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-br-vis-front/static/js/2.b629d9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a9217433054a6b1216",
+    "url": "/covid19-br-vis-front/static/js/main.73e42330.chunk.js"
+  },
+  {
+    "revision": "ed12673fbc9b72d06631",
+    "url": "/covid19-br-vis-front/static/js/runtime-main.3815174a.js"
+  }
+]);
