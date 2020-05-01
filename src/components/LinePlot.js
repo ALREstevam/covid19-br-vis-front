ecd7484c5c2db8b57841d1c7ffb92bab31238b6a
ALREstevam <a166348@dac.unicamp.br>
2020-05-01 01:24:32 -0300

    adding plus or minus last day

--- src/components/LinePlot.js
+++ src/components/LinePlot.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ResponsiveLine } from '@nivo/line'
 
 
-function formatNumber(num, decimalPlaces=2) {
+function formatNumber(num, decimalPlaces = 2) {
     if (typeof num === 'string' || num instanceof String) {
         return num
     }
@@ -14,40 +14,58 @@ function formatNumber(num, decimalPlaces=2) {
     }
 }
 
-function showEvery(n){
+function showEvery(n) {
     let current = 0
     return (value) => {
-      current += 1
-      if(current % n === 0){
-        current = 0
-        return value
-      }else{
-        return ''
-      }
+        current += 1
+        if (current % n === 0) {
+            current = 0
+            return value
+        } else {
+            return ''
+        }
     }
 }
 
 const showSometimes = showEvery(3)
 
 
-const LinePLot = ({ data, colors={scheme: 'set3'}, stacked=false }) => (
-    <ResponsiveLine
+const LinePLot = ({ data, colors = { scheme: 'set3' }, stacked = false }) => {
+    return (<ResponsiveLine
         data={data}
         margin={{ top: 50, right: 60, bottom: 100, left: 60 }}
-        tooltip={(node) => (
-            <div style={{
-                color: node.point.serieColor,
-                backgroundColor: '#fff',
-                padding: '5px',
-                border: `1px solid ${node.point.borderColor}`,
-                textAlign: 'center',
-                borderRadius: '5px',
-                boxShadow: '5px 5px 10px -7px rgba(0,0,0,0.75)'
-            }}>
-                <strong style={{fontSize: '.9em'}}>{node.point.data.xFormatted}</strong><br />
-                <span>{node.point.serieId}: <strong>{node.point.data.yFormatted}</strong></span>
-            </div>
-        )}
+        tooltip={(node) => {
+            let last = undefined
+            let lasty = 0
+
+            let index = Number.parseInt(node.point.id.split('.')[1])
+            if(index > 0){
+                last = data.filter(data => data.id === node.point.serieId)[0].data[index-1]
+                lasty = last.y
+            }
+            
+            let lastDiff = node.point.data.yFormatted - lasty
+            let lastDiffStr = formatNumber(lastDiff)
+            lastDiffStr = lastDiff >= 0 ? `+${lastDiffStr}` : `${lastDiffStr}`   
+            
+
+            return (
+                <div style={{
+                    color: node.point.serieColor,
+                    backgroundColor: '#fff',
+                    padding: '5px',
+                    border: `1px solid ${node.point.borderColor}`,
+                    textAlign: 'center',
+                    borderRadius: '5px',
+                    boxShadow: '5px 5px 10px -7px rgba(0,0,0,0.75)'
+                }}>
+                    <strong style={{ fontSize: '.9em' }}>{node.point.data.xFormatted}</strong><br />
+                    <span>{node.point.serieId}: <strong>{node.point.data.yFormatted}</strong>
+                    <span> ({lastDiffStr})</span>
+                    </span>
+                </div>
+            )
+        }}
         xScale={{
             type: 'time', //point, time, linear
             //min: 0,
@@ -116,7 +134,8 @@ const LinePLot = ({ data, colors={scheme: 'set3'}, stacked=false }) => (
                 ]
             }
         ]}
-    />
-)
+    />)
+}
+
 
 export default LinePLot
